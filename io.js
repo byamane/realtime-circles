f02b0b8eb6224cda7ddb1f711903d71c6b332a2a
byamane <byamane21@gmail.com>
2022-05-14 12:19:22 -0500

    configure server to listen for client messages and emit them to all other clients

--- io.js
+++ io.js
@@ -2,7 +2,9 @@ const io = require('socket.io')();
 
 // Listen for new connections from clients (socket)
 io.on('connection', function (socket) {
-  console.log('Client connected to socket.io!')
+  socket.on('add-circle', function (data) {
+    io.emit('add-circle', data)
+  })
 })
 
 // io represents socket.io on the server - let's export it
